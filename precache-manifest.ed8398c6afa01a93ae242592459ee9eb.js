2cacc2986d51f79b50929e6a20e407efba97e72a
darq37 <dzmudziak@gmail.com>
2020-02-19 19:51:50 +0100

    Updates

---
+++ precache-manifest.ed8398c6afa01a93ae242592459ee9eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889d1a0376853d7338880545280ed184",
+    "url": "/kamid/index.html"
+  },
+  {
+    "revision": "84588743600e77df41a2",
+    "url": "/kamid/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ca91012d6b013d0771c5",
+    "url": "/kamid/static/js/2.669a4153.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kamid/static/js/2.669a4153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84588743600e77df41a2",
+    "url": "/kamid/static/js/main.58e4e903.chunk.js"
+  },
+  {
+    "revision": "525b07c1b892c5cebcf9",
+    "url": "/kamid/static/js/runtime-main.84a25b99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kamid/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/darq37/kamid/precache-manifest.fbbb87745a5b89054f5b7c419032fda3.js"
+  "/kamid/precache-manifest.ed8398c6afa01a93ae242592459ee9eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/darq37/kamid/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kamid/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
